a6d3ac31a9504a057851ac037a1d183185802817
aclopes <aclopes@gmail.com>
2025-04-04 18:21:46 -0400

    Program runing

--- MouseMove/MouseMove.ino
+++ MouseMove/MouseMove.ino
@@ -8,7 +8,7 @@ Sensor will check the presence and move the servo drive.
 // Variable to change detect and change the status
 int val = 0;
 int count = 0;
-int delayMS = 60000;
+int delayMS = 60;
 int pos = 0;    // variable to store the servo position
 Servo myservo;  // create Servo object to control a servo
 
@@ -18,6 +18,7 @@ void setup() {
   pinMode(pirPin, INPUT);
   // Begin serial communication at a baud rate of 9600:
   Serial.begin(9600);
+  digitalWrite(LED_BUILTIN, LOW);
 
 }
 
@@ -31,11 +32,23 @@ void loop() {
     count = 0;
   }
 
-  if (count >= delayMS){
+  if (count > delayMS){
     Serial.println("Motion not detected prepare to move");
+    Serial.println(count);
     digitalWrite(LED_BUILTIN, HIGH);
+    //move the servo every 30 degrees
+    myservo.write(pos); 
+    
+    //when in half circle, restart
+    if (pos >= 180){
+      pos = 0;
+    }
+
+    // Couters
+    pos += 30;
     count = 0;
   }
 
   count+= 1;
+  delay(1000);
 }
